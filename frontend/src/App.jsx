f1e33f096a46ee2dc93a3d5d39c0bebb82f54309
Ouballa10 <abdelmounaimoub12@gmail.com>
2026-05-25 17:01:07 +0200

    build new design principl intergrer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import "./styles/profile-new.css";
 import "./styles/doc-section.css";
 import "./styles/qr-scanner.css";
 import "./styles/dark-mode.css";
+import "./styles/luxury.css";
 import "./styles/home-luxury.css";
 
 function OfflineNotice() {
